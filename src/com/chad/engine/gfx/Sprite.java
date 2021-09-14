1105ac7f90a5e310c5cc5960873b12dd447ecf43
Student <Student@118-8.local>
2021-09-14 11:37:40 -0400

    Reading tiles from file started

--- src/com/chad/engine/gfx/Sprite.java
+++ src/com/chad/engine/gfx/Sprite.java
@@ -1,7 +1,5 @@
 package com.chad.engine.gfx;
 
-import com.chad.engine.Game;
-
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -11,28 +9,17 @@ public class Sprite {
     // sprite size in pixels
     public int width, height;
 
-    // sprite pixels, mapped into [0..3] space
-    public int[] pixels;
+    // image
+    public BufferedImage image;
 
     public Sprite(String path) {
-        BufferedImage image;
-
         try {
-            image = ImageIO.read(Objects.requireNonNull(Game.class.getResourceAsStream(path)));
+            image = ImageIO.read(Objects.requireNonNull(Sprite.class.getResourceAsStream(path)));
         } catch (IOException e) {
             throw new Error(e);
         }
 
         this.width = image.getWidth();
         this.height = image.getHeight();
-        this.pixels = new int[this.width * this.height];
-
-        int[] imagePixels = image.getRGB(
-            0, 0, image.getWidth(), image.getHeight(),
-            null, 0, image.getWidth());
-
-        for (int i = 0; i < this.width * this.height; i++) {
-            this.pixels[i] = ((imagePixels[i] >> 24) & 0xFF) != 0xFF ? -1 : (imagePixels[i] & 0xFF) / 64;
-        }
     }
 }
--- src/com/chad/engine/tile/TileMap.java
+++ src/com/chad/engine/tile/TileMap.java
@@ -1,46 +1,115 @@
 package com.chad.engine.tile;
 
 import com.chad.engine.entity.Entity;
+import com.chad.engine.gfx.Renderer;
+import com.chad.engine.gfx.Spritesheet;
 import com.chad.engine.utils.GameFile;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class TileMap extends Entity {
 
+    // sprite sheet and tile types
+    private Spritesheet spritesheet;
     private short[][] tiles;
+
+    // width and height in numbers of tiles
+    private int tx;
+    private int ty;
+
+    // tile size
     private int tileSize;
 
-    private void getTiles(String filePath) throws Exception {
-        // first 3 lines are width, height, and tile size
-        // after that, each tile is separated by a space
+    public TileMap() { this(0, 0, 0); }
+
+    public TileMap(int tx, int ty, int tileSize) {
+        super();
+
+        this.tx = tx;
+        this.ty = ty;
+        this.tileSize = tileSize;
+    }
+
+    private void getTiles(String filePath) {
+        // get lines
         ArrayList<String> lines = new GameFile(filePath).readLines();
-        width = Integer.parseInt(lines.get(0));
-        height = Integer.parseInt(lines.get(1));
-        tileSize = Integer.parseInt(lines.get(2));
 
         // there should be 3 lines plus the height
         assert lines.size() == height + 3;
 
-        // read tiles from each line
+        // first 3 lines are x tiles, y tiles, and tile size
+        tx = Integer.parseInt(lines.get(0));
+        ty = Integer.parseInt(lines.get(1));
+        tileSize = Integer.parseInt(lines.get(2));
+
+        // twidth and theight should be positive
+        assert tx > 0 && ty > 0;
+
+        // create tiles array
+        tiles = new short[tx][ty];
+
+        // read tiles from each lines where numbers are separated by spaces
+        for (int i = 0; i < ty; i++) {
+
+            // get the line, create a new array for the tiles
+            String line = lines.get(i + 3);
+            short[] tileLine = new short[tx];
+
+            // index and value variables to keep track of the current tile in the string
+            int index = 0;
+            int value = 0;
+
+            // loop through each character to get the current tile value
+            for (int j = 0; j < line.length(); j++) {
+
+                // move on once a space character has been reached
+                if (line.charAt(j) == ' ') {
+                    tileLine[index] = (short) value;
+                    index++;
+                    value = 0;
+                    continue;
+                }
+
+                // otherwise, shift the previous value by 10 and add the new value
+                value = value * 10 + line.charAt(j) - 48;
+            }
+
+            // add this to the tiles array
+            tiles[i] = tileLine;
+        }
     }
 
     @Override
-    public void update(float dt) {
-
-    }
+    public void update(float dt) {}
 
     @Override
     public void draw() {
+        // get offset based on the position of the tilemap
+        float ox = getX();
+        float oy = getY();
 
+        oy += tileSize;
+
+        // draw each tile
+        for (int x = 0; x < tx; x++)
+            for (int y = 0; y < ty; y++) {
+                Renderer.setColor(tiles[x][y] == 0 ? Color.gray : Color.white);
+                Renderer.fill(y * tileSize + ox, x * tileSize + oy, tileSize, tileSize);
+            }
+    }
+
+    public boolean checkTileCollisions(Entity other) {
+        // TODO
+        return false;
     }
 
     @Override
     public boolean setAttribute(String attr, String value) throws Exception {
-        if (attr.equals("src")) {
-            getTiles(value);
-            return true;
+        switch (attr) {
+            case "src":
+                getTiles(value);
+                return true;
         }
         return super.setAttribute(attr, value);
     }
